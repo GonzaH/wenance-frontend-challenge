1970c76a59a6a880399bf174f121af803915d79a
Gonzalo <gonzalo.hernandorena@cloudhesive.com>
2021-08-12 13:28:26 -0300

    feat - Styled and turned into form the filter

--- src/Components/Atoms/Filter.js
+++ src/Components/Atoms/Filter.js
@@ -1,20 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux'
+import styled from 'styled-components';
 
 import { FILTER_PLACEHOLDER } from '../../utils/constants';
 import { applyPersonFilter } from '../../redux/actions/peopleActions';
+
+const Form = styled.form`
+  margin: 1em;
+`;
+
+const Input = styled.input`
+  margin-right: .5em;
+  font-size: 1.4em;
+  background: #A6ECD0;
+  border-radius: 7px;
+`;
+
+const Button = styled.button`
+  font-size: 1.4em;
+  background: #A6ECD0;
+  border-radius: 7px;
+`;
 
 const Filter = () => {
   const [filterContent, setFilterContet] = useState('');
   const dispatch = useDispatch();
 
-  return (<div>
-    <input
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(applyPersonFilter(filterContent.toLowerCase()));
+  }
+
+  return (<Form onSubmit={handleSubmit}>
+    <Input
       placeholder={FILTER_PLACEHOLDER}
       onChange={({ target: { value } }) => setFilterContet(value)} value={filterContent}
     />
-    <button onClick={() => dispatch(applyPersonFilter(filterContent.toLowerCase()))}>Search</button>
-  </div>);
+    <Button type="submit">Search</Button>
+  </Form>);
 };
 
 export default Filter;
