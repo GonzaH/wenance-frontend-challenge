b1fc03265cef2263e16ec08ae3555186283fda16
Gonzalo <gonzalo.hernandorena@cloudhesive.com>
2021-08-12 13:43:42 -0300

    feat - Made the filter responsive

--- src/Components/Atoms/Filter.js
+++ src/Components/Atoms/Filter.js
@@ -7,6 +7,11 @@ import { applyPersonFilter } from '../../redux/actions/peopleActions';
 
 const Form = styled.form`
   margin: 1em;
+
+  @media only screen and (max-device-width: 700px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const Input = styled.input`
@@ -14,12 +19,25 @@ const Input = styled.input`
   font-size: 1.4em;
   background: #A6ECD0;
   border-radius: 7px;
+
+  @media only screen and (max-device-width: 700px) {
+    margin-right: 0;
+    font-size: 1em;
+    margin-bottom: .5em;
+  }
 `;
 
 const Button = styled.button`
   font-size: 1.4em;
   background: #A6ECD0;
   border-radius: 7px;
+
+  @media only screen and (max-device-width: 700px) {
+    font-size: 1em;
+    width: max-content;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 const Filter = () => {
